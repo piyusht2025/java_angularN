c49cbd5ff655fcce6a4ef4b4f5904c056e8492f7
Piyush Tarai <piyush.tarai@argusoft.com>
2025-08-05 14:14:28 +0530

    ..

--- JAVA/Exercises/Exercise_1/src/abstractShape/Main.java
+++ JAVA/Exercises/Exercise_1/src/abstractShape/Main.java
@@ -1,6 +1,72 @@
 package abstractShape;
 
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+abstract class shape{
+    abstract double calculateArea();
+}
+
+class circle extends shape {
+    double radius;
+    circle(double r){
+
+        radius=r;
+    }
+    @Override
+    double calculateArea() {
+        return (Math.PI*radius*radius);
+    }
+}
+class rectangle extends shape {
+    double length;
+    double breadth;
+    rectangle(double l,double b){
+        length=l;
+        breadth=b;
+    }
+    @Override
+    double calculateArea() {
+        return length*breadth;
+    }
+}
+class square extends shape {
+    double side;
+    square(double side){
+        this.side=side;
+    }
+
+    @Override
+    double calculateArea() {
+        return side*side;
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Shape Circle/Rectangle/Square or 0 for exit :");
+        String s = sc.next();
+        while(!s.equals("0") ) {
+            if (s.equals("Circle")) {
+                System.out.println("Enter radius : ");
+                circle c = new circle(sc.nextDouble());
+                System.out.println("Circle Area = " + c.calculateArea());
+            } else if (s.equals("Rectangle")) {
+                System.out.println("Enter Length Breadth : ");
+                rectangle r = new rectangle(sc.nextDouble(), sc.nextDouble());
+                System.out.println("rectangle Area = " + r.calculateArea());
+            } else if (s.equals("Square")) {
+                System.out.println("ENter Side ");
+                square sq = new square(sc.nextDouble());
+                System.out.println("Area of square = " + sq.calculateArea());
+            } else {
+                System.out.println("Enter Valid shape");
+            }
+            System.out.println("Enter Shape Circle/Rectangle/Square or 0 for exit :");
+            s = sc.next();
+        }
+        System.out.println("Exit");
+    }
     /* Objective:
 Model different geometric shapes using abstraction and calculate their areas.
 
