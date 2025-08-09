a66282e5b728310061111c348885e00e57144faf
Piyush Tarai <piyush.tarai@argusoft.com>
2025-08-09 17:18:16 +0530

    Multithreading updated

--- JAVA/Exercises/Exercise_3_Multithreading/src/Multithreading/CustomerOrder.java
+++ JAVA/Exercises/Exercise_3_Multithreading/src/Multithreading/CustomerOrder.java
@@ -1,23 +1,28 @@
 package Multithreading;
 
+//import static Multithreading.Main.OrdersRemaining;
+
 public class CustomerOrder extends Thread{
     private int orderNumber;
+    static Integer OrdersRemaining=4;
    // private int s =0;
-    public CustomerOrder(String s, int orderNumber) {
+    public CustomerOrder(String s, int orderNumber,int OrdersRemaining) {
         super(s);
         this.orderNumber = orderNumber;
+       // this.OrdersRemaining = OrdersRemaining;
     }
-    public void  run() {
-        synchronized (this){
-            try {
-                System.out.println(Thread.currentThread().getName() + " received order no." + orderNumber);
-                System.out.println(Thread.currentThread().getName() + " Processing order...");
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
 
-            System.out.println(Thread.currentThread().getName() + ": Processing complete...." );
+    public  void  run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " received order no." + orderNumber);
+            System.out.println(Thread.currentThread().getName() + " Processing order..."+ orderNumber);
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
         }
-    }
+        synchronized (OrdersRemaining){
+        System.out.println(Thread.currentThread().getName() + ": Processing complete...."+ orderNumber +" Orders Remaining : "+OrdersRemaining--);
+//        OrdersRemaining--;
+//        System.out.println(Thread.currentThread().getName() + ": Processing complete...."+ orderNumber);
+    }}
 }
--- JAVA/Exercises/Exercise_3_Multithreading/src/Multithreading/Main.java
+++ JAVA/Exercises/Exercise_3_Multithreading/src/Multithreading/Main.java
@@ -1,13 +1,16 @@
 package Multithreading;
 
+
 public class Main {
+    public static Integer OrdersRemaining=4;
     public static void main(String[] args) {
-        CustomerOrder order1 = new CustomerOrder("Processor0", 0);
-        CustomerOrder order2 = new CustomerOrder("Processor1", 1);
-        CustomerOrder order3 = new CustomerOrder("Processor2", 2);
-        CustomerOrder order4 = new CustomerOrder("Processor3", 3);
-        CustomerOrder order5 = new CustomerOrder("Processor4", 4);
+        CustomerOrder order1 = new CustomerOrder("Processor0", 0,OrdersRemaining);
+        CustomerOrder order2 = new CustomerOrder("Processor1", 1,OrdersRemaining);
+        CustomerOrder order3 = new CustomerOrder("Processor2", 2,OrdersRemaining);
+        CustomerOrder order4 = new CustomerOrder("Processor3", 3,OrdersRemaining);
+        CustomerOrder order5 = new CustomerOrder("Processor4", 4,OrdersRemaining);
         CustomerOrder[] orders ={order1,order2,order3,order4,order5};
+//        OrdersRemaining=orders.length;
 
 
         for(int i=0; i<5; i++){
@@ -19,6 +22,8 @@ public class Main {
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
+
+//            System.out.println("Orders Remaining: "+OrdersRemaining--);
         }
 
     }
